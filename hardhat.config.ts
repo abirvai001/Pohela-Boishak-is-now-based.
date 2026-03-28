8260a4da639a4ad77fea9aabf57f6b373c8b8400
0xluffy <badhonusa1@gmail.com>
2026-03-28 18:54:46 +0600

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,1 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import "dotenv/config";
 
+const ALCHEMY_URL = process.env.BASE_RPC || "";
+const PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    base: {
+      url: ALCHEMY_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
