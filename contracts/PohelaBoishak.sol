ff9dc4986a8fd601f614264294208c95884978a8
0xluffy <badhonusa1@gmail.com>
2026-03-28 18:38:31 +0600

    Update PohelaBoishak.sol

--- contracts/PohelaBoishak.sol
+++ contracts/PohelaBoishak.sol
@@ -9,3 +9,22 @@ contract PohelaBoishak {
         emit BoishakGreeting(msg.sender, to, message, block.timestamp);
     }
 }
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import "dotenv/config";
+
+const ALCHEMY_URL = process.env.BASE_RPC || "";
+const PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    base: {
+      url: ALCHEMY_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
