187eab696e8f487c8003c48b4b6b979f51330f14
0xluffy <badhonusa1@gmail.com>
2026-03-28 18:39:01 +0600

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,1 +1,13 @@
+import { ethers } from "hardhat";
 
+async function main() {
+  const PohelaBoishak = await ethers.getContractFactory("PohelaBoishak");
+  const pb = await PohelaBoishak.deploy();
+  await pb.deployed();
+  console.log("PohelaBoishak deployed to:", pb.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
